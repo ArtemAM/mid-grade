a95b49a0dedd68f48be72020603714d324e16964
Artem.Mokeev <artmokeev274@gmail.com>
2025-05-22 14:59:31 +0300

    решение js/task6

--- js/task6/task.js
+++ js/task6/task.js
@@ -1,12 +1,20 @@
 function mapWithReduce(arr, callback) {
-  // Решение ...
+  return arr.reduce((acc, el) => {
+    acc.push(callback(el));
+    return acc;
+  }, []);
 }
 
 function doubleIt(element) {
-  return element * 2;
+  if (typeof element === "number") {
+    return element * 2;
+  }
+  if (typeof element === "string") {
+    return element.repeat(2);
+  }
 }
 
 const elements = [322, 1337, "foo", -21, "bar"];
 
 console.log(elements.map((el) => doubleIt(el))); // для проверки
-console.log(mapWithReduce(elements, mapWithReduce));
+console.log(mapWithReduce(elements, doubleIt));
